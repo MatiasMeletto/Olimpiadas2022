d43e5a5a0b3cd1ba3c6aac58ea9ad496ffc2aa4b
info-4 <info-4@INFO4-PC3>
2022-09-14 09:14:46 -0300

    Agregar archivos de proyecto.

---
+++ Musse/Data/MuseoingContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Musse.Data
+{
+    public class MuseoingContext : IdentityDbContext
+    {
+        public MuseoingContext(DbContextOptions<MuseoingContext> options)
+            : base(options)
+        {
+        }
+    }
+}
