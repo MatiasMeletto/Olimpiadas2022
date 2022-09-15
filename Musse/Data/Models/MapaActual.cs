49e63334490e3abd17ea576c6297ece57809d569
Matias <INFO-4@INFO2-PC9>
2022-09-15 10:17:24 -0300

    Agregado de atributos en Mapas

--- Musse/Data/Models/MapaActual.cs
+++ Musse/Data/Models/MapaActual.cs
@@ -3,6 +3,8 @@
     public class MapaActual
     {
         public int MapaActualId { get; set; }
-        public string MapaActualBase{ get; set; }
+        public string MapaActualBase { get; set; }
+        public string Tipo { get; set; }
+        public string Nombre { get; set; }
     }
 }
--- Musse/Data/Models/MapaPrevio.cs
+++ Musse/Data/Models/MapaPrevio.cs
@@ -4,5 +4,7 @@
     {
         public int MapaPrevioId { get; set; }
         public string MapaPrevioBase { get; set; }
+        public string Tipo { get; set; }
+        public string Nombre { get; set; }
     }
 }
