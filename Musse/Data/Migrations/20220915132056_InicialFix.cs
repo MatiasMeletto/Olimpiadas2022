3f8d60170da25683405a0a1fb07e8cd9f7fd56ba
Matias <INFO-4@INFO2-PC9>
2022-09-15 10:21:30 -0300

    Migracion InicialFIx

---
+++ Musse/Data/Migrations/20220915132056_InicialFix.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Musse.Data.Migrations
+{
+    public partial class InicialFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Nombre",
+                table: "MapaPrevios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tipo",
+                table: "MapaPrevios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nombre",
+                table: "Mapa",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tipo",
+                table: "Mapa",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nombre",
+                table: "MapaPrevios");
+
+            migrationBuilder.DropColumn(
+                name: "Tipo",
+                table: "MapaPrevios");
+
+            migrationBuilder.DropColumn(
+                name: "Nombre",
+                table: "Mapa");
+
+            migrationBuilder.DropColumn(
+                name: "Tipo",
+                table: "Mapa");
+        }
+    }
+}
