48d83c05523eac62ddbc77df752d75422ae34a48
Matías Meletto <mmeletto@eest1chivilcoy.edu.ar>
2022-09-15 13:44:04 -0300

    Cambio en context para la factory

--- Musse/Data/MuseoingContext.cs
+++ Musse/Data/MuseoingContext.cs
@@ -8,14 +8,23 @@ namespace Musse.Data
     {
         public DbSet<MapaActual> Mapas { get; set; }
         public DbSet<Recorrido> Recorridos { get; set; }
+
         public MuseoingContext(DbContextOptions<MuseoingContext> options)
             : base(options)
         {
         }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public void ConfigureServices(IServiceCollection services)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true");
+            services.AddDbContextFactory<MuseoingContext>(
+                options =>
+                    options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true"));
+        }
+
+        private readonly IDbContextFactory<MuseoingContext> _contextFactory;
+
+        public MyController(IDbContextFactory<MuseoingContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
         }
     }
 }
