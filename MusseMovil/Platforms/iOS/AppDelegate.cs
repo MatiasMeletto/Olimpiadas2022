a1a1d4fdaa494ac747d7658cb47c0f0960138421
Matías Meletto <mmeletto@eest1chivilcoy.edu.ar>
2022-09-16 16:08:57 -0300

    Agregado de MusseMovil(nuevo proyecto .NET Maui)

---
+++ MusseMovil/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MusseMovil
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
