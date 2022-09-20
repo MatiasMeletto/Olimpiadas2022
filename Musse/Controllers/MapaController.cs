1ec5297150f8d31548fb8e9342137fa7e3961677
Matías Meletto <mmeletto@eest1chivilcoy.edu.ar>
2022-09-20 16:21:48 -0300

    Agregado de esta rama final y probando como seria leer o subir datos

---
+++ Musse/Controllers/MapaController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Musse.Data;
+using Musse.Data.Models;
+using MusseCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Musse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MapaController : ControllerBase
+    {
+        private readonly MuseoingContext _dbContext;
+
+        public MapaController(MuseoingContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/<MapaController>
+        [HttpGet]
+        public IEnumerable<MapaDTO> Get()
+        {
+            MapaActual? mapa = _dbContext.Mapas.Where(m => m.Actual).AsNoTracking().SingleOrDefault();
+            List<MapaDTO> mapasDTO = new List<MapaDTO>();
+            if (mapa is not null)
+            {
+                MapaDTO mapaDTO = new MapaDTO()
+                {
+                    MapaBase64 = mapa.MapaBase64,
+                    Tipo = mapa.Tipo,
+                    Nombre = mapa.Nombre,
+                };
+                mapasDTO.Add(mapaDTO);
+            }
+            return mapasDTO;
+        }
+
+        // GET api/<MapaController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<MapaController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        // PUT api/<MapaController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<MapaController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Musse/Controllers/ResenasController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Musse.Data;
+using Musse.Data.Models;
+using MusseCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Musse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResenasController : ControllerBase
+    {
+        private readonly MuseoingContext _dbContext;
+
+        public ResenasController(MuseoingContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/<ValuesController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/<ValuesController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] ResenaDTO value)
+        {
+            ResenaComentario resena = new ResenaComentario()
+            {
+                Fecha = value.Fecha,
+                ResenaComentarioString = value.ResenaComentarioString,
+                Nota = (TipoNota)value.Nota
+            };
+            _dbContext.ResenasComentarios.Add(resena);
+            _dbContext.SaveChanges();
+        }
+
+        // PUT api/<ValuesController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<ValuesController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
